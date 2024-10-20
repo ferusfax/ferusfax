0261b8e114e35f4ec706c5d4c63642b9914fb84a
Helber Fernandes <helberfernandes2000@gmail.com>
2024-10-20 17:16:11 -0300

    fix: remove code

--- packages/ferusfax/src/services/plugin/pluginsService.ts
+++ packages/ferusfax/src/services/plugin/pluginsService.ts
@@ -313,9 +313,6 @@ export class PluginService implements IPluginService {
   async runPlugin(options) {
     if (cluster.isPrimary) {
       cluster.fork();
-      cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-      });
     } else {
       await this.runPluginAsWorker(options);
     }
